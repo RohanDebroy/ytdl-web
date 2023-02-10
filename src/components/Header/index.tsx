1ab618cecea7ab83f7eefc5dd82d8c1487401510
Rohan Debroy <debroy.debroy2017@gmail.com>
2023-02-11 00:15:10 +0530

    Added UI for Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,45 @@
+import {
+  AppBar,
+  Container,
+  Toolbar,
+  Link as MuiLink,
+  styled,
+} from "@mui/material";
 import React, { FC } from "react";
 
+const Link = styled(MuiLink)({
+  backgroundImage: "linear-gradient(45deg, orange, purple)",
+  backgroundClip: "text",
+  textFillColor: "transparent",
+  cursor: "pointer",
+});
+
 const Header: FC = () => {
-  return <header></header>;
+  return (
+    <AppBar position="static" color="transparent">
+      <Container maxWidth="md">
+        <Toolbar>
+          <Link
+            underline="none"
+            href="/"
+            sx={{
+              mr: "auto",
+            }}
+          >
+            Youtube DL
+          </Link>
+          <Link
+            href="https://github.com/RohanDebroy"
+            target="__blank"
+            rel="noopener"
+            underline="none"
+          >
+            Github
+          </Link>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 };
 
 export default Header;
