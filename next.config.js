ae8edd7b669a23f31eb5a57f12c20bb2bc92fee6
Rohan Debroy <debroy.debroy2017@gmail.com>
2023-02-11 11:49:55 +0530

    Updated next config for local testing

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  /**
+   * INFO: Uncomment below code to route all api through the python backend
+   * only to be used for local testing,make sure the destination is correct.
+   * For vercel deployments it is not necessary
+   */
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/api/:path*",
+  //       destination: "http://127.0.0.1:8000/api/:path*",
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
