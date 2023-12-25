b4b93590e95fc0a8216585396f18b9ba524916d4
Rohan Debroy <debroy.debroy2017@gmail.com>
2023-12-25 10:47:05 +0530

    Add shadcn/ui config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body>{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <h1 className="outline outline-4">ytdl-web!</h1>;
+  return <h1>ytdl-web!</h1>;
 }
