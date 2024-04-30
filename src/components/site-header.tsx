8e49780846ba807d5e5162a26212e6dbded89f11
Rohan Debroy <debroy.debroy2017@gmail.com>
2024-04-30 22:52:02 +0530

    Add gradient to the link

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -4,15 +4,16 @@ import { siteConfig } from "@/config/site";
 import { cn } from "@/utils/helpers";
 import { Github } from "@/components/icons";
 import { ThemeToggle } from "./theme-toggle";
-import { Search } from "lucide-react";
-import { Input } from "./ui/input";
 import { buttonVariants } from "./ui/button";
 
 export function SiteHeader() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center">
-        <Link href="/" className="mr-auto flex items-center space-x-2">
+        <Link
+          href="/"
+          className="mr-auto flex items-center space-x-2 bg-gradient-to-tr from-orange-500 to-purple-500 bg-clip-text [-webkit-text-fill-color:transparent] text-2xl"
+        >
           <span className="font-bold sm:inline-block">{siteConfig.name}</span>
         </Link>
         <nav className="flex items-center">
