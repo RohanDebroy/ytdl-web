9ff211cd8d0b8cf1a6507275b0f0a365d3233b45
Rohan Debroy <debroy.debroy2017@gmail.com>
2024-05-05 22:58:51 +0530

    Feat: Search page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Search } from "lucide-react";
+
 export default function Page() {
-  return <h1>ytdl-web!</h1>;
+  return (
+    <div className="container grid place-items-center">
+      <form
+        className="inline-flex w-full max-w-[30rem] space-x-2"
+        action="/result"
+      >
+        <Input type="search" name="url" placeholder="enter url..." required />
+        <Button type="submit" size="icon">
+          <Search />
+        </Button>
+      </form>
+    </div>
+  );
 }
